157f9f9d8796d2f74009fdc2edb7e5255058b167
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2026-09-22 13:14:44 +0300

    Import openpyxl in training_01.py
    Added import statement for openpyxl.

--- training_01.py
+++ training_01.py
@@ -10,6 +10,7 @@ Original file is located at
 import pandas as pd
 import numpy as np
 import streamlit as st
+Import openpyxl
 from datetime import date, datetime, timedelta
 
 Today = date.today()
